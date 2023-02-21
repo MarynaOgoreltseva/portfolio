ac9f40490543286cbb1ff21834faadc7c37e4beb
MarynaOgoreltseva <ogoreltseva1@gmail.com>
2023-02-21 12:30:31 +0200

    menu cross

--- 2rism/js/menu.js
+++ 2rism/js/menu.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('.nav__burger').click(function() {
-        $('.nav_mobile,.wrap_header').toggleClass('is-active');
-        $('.btn-burger').toggleClass('open');
+        $('.nav_mobile,.wrap_header,.btn-burger,.nav__burger').toggleClass('is-active');
     });
 });
