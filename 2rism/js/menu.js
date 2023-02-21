fb04372ae0c18476f76a4a1905e47d08b2a4c1ed
MarynaOgoreltseva <ogoreltseva1@gmail.com>
2023-02-21 12:09:57 +0200

    cross is ready

--- 2rism/js/menu.js
+++ 2rism/js/menu.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.nav__burger').click(function() {
         $('.nav_mobile,.wrap_header').toggleClass('is-active');
+        $('.btn-burger').toggleClass('open');
     });
 });
